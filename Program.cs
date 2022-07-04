591211cd363beace6848c580b0df358e64c4b58a
Alexandra <sasha_huliganka@mail.ru>
2022-07-04 13:15:18 +0300

    add read me file

--- Program.cs
+++ Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк,
+// длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры,
+// либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями,
+// лучше обойтись исключительно массивами.
+
+// **Примеры**:
+
+// ["hello", "2", "world", ":-)"] -> ["2", ":-)"]
+
+// ["1234", "1567", "-2", "computer science"] -> ["-2"]
+
+// ["Russia", "Denmark", "Kazan"] -> []
+
+
+// string[] arrayString = new string[] {"summer", "spring", "autumn", "sun", "day"};
+
+// var result = new string[arrayString.Length];
+// var realSize = 0;
+// for (int i = 0; i < arrayString.Length; i++)
+// {
+// 	if(arrayString[i].Length <= 3)
+// 	{
+// 		result[realSize] = arrayString[i];
+// 		Console.Write($"{result[realSize]}, ");
+// 		realSize++;
+// 	}
+// }
+
+string[] arrayString = new string[] {"summer", "spring", "autumn", "sun", "day"};
+
+string FindLenght(string[] array)
+{
+	int size = 0;
+	for (int i = 0; i < array.Length; i++)
+	{
+		if (array[i].Length <= 3)
+		{
+			size++;
+		}
+	}
+	return Convert.ToString(size);
+}
+
+int lenghthArr = Convert.ToInt16(FindLenght(arrayString));
+
+string[] newArray = new string[lenghthArr];
+
+string[] FillArray(string[] arrOne, string[] arrTwo)
+{
+	int index = 0;
+	for (int i = 0; i < arrOne.Length; i++)
+	{
+		if(arrOne[i].Length <= 3)
+		{
+			arrTwo[index] = arrOne[i];
+			index++;
+		}
+	}
+	return arrTwo;
+}
+
+Console.WriteLine(FillArray(arrayString, newArray));
